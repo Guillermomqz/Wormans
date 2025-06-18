7b67279d6986fe0a173b524ff5c4b96e7bcea0fe
Guillermomqz <mariashadow98k@gmail.com>
2025-06-17 22:39:08 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-function compartir() {
-  const url = encodeURIComponent(window.location.href);
-  const mensaje = encodeURIComponent("Mira este video en Worman's");
-  window.open(`https://www.facebook.com/sharer/sharer.php?u=${url}`, '_blank');
-  window.open(`https://api.whatsapp.com/send?text=${mensaje}%20${url}`, '_blank');
-}
